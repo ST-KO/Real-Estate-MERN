32bd022f864fc083fa298c63dcd6d21e2abfbca5
ST-Ko <sithu.ko21120@gmail.com>
2024-06-09 12:38:24 +1000

    add userschema and controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
@@ -25,6 +26,53 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {};
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-export const logout = (req, res) => {};
+    // CHECK IF THE USER EXISTS
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid Credentials" });
+    }
+
+    // CHECK USER PASSWORD IS CORRECT
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid Credentials" });
+    }
+
+    // GENERATE COOKIE TOKEN AND SEND TO THE USER
+    // res.setHeader("Set-Cookie", "test=" + "myValue").json("Success");
+    const age = 1000 * 60 * 60 * 24 * 7; // 1ms * a minute * a hour * a day * 7 days
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: age,
+    });
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        //   secure: true,
+        maxAge: age, // Set token expiration
+      })
+      .status(200)
+      .json({ message: "Login Successful" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to login" });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token").status(200).json({ message: "Logout Successful" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to logout" });
+  }
+};

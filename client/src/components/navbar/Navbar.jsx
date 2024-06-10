aacc294022b976ea53c5c1c286e136182ca8ba57
ST-Ko <sithu.ko21120@gmail.com>
2024-06-10 15:28:51 +1000

    navbar added

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.scss';
 
 const Navbar = () => {
+  
+  const [open, setOpen] = useState(false);
+  
   return (
     <nav>
         <div className='left'>
-          <a href="/">
+          <a href="/" className='logo'>
             <img src="/logo.png" alt="logo" />
+            <span>LamaEstate</span>
           </a>
           <a href="/">Home</a>
           <a href="/">About</a>
           <a href="/">Others</a>
+          <a href="/">Agents</a>
         </div>
         <div className='right'>
-          <a href="/">Agents</a>
           <a href="/">Sign In</a>
-          <a href="/">Sign Out</a>
+          <a href="/" className='register'>Sign Out</a>
+          <div className='menuIcon'>
+            <img 
+              src="/menu.png" 
+              alt="menu icon" 
+              onClick={() => setOpen(prevState => !prevState)} 
+            />
+          </div>
+          <div className={open ? 'menu active' : 'menu'}>
+            <a href="/">Home</a>
+            <a href="/">About</a>
+            <a href="/">Others</a>
+            <a href="/">Agents</a>
+            <a href="/">Sign In</a>
+            <a href="/">Sign Up</a>
+          </div>
         </div>
     </nav>
   );
